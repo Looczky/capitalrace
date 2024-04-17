fa2333ddc667393b12bf6223bf961a0311eb61c5
Looczky <kwiatkowskiluk@protonmail.com>
2024-04-17 22:46:50 +0200

    frontend + skip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Game from './Game';
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 function App() {
   return (
--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Stopwatch from './Stopwatch';
+import shuffle from './utils';
 
 function Game() {
   const [remaining, setRemaining] = useState(10);
@@ -11,7 +12,9 @@ function Game() {
   const [currentCountry, setCurrentCountry] = useState('');
   const [currentCapital, setCurrentCapital] = useState('');
   const [pauseTimer,setPauseTimer] = useState(true);
+  const [skipCapital,setSkipCapital] = useState(false);
   
+
   function handleInputChange(event){
     setInputValue(event.target.value);
   }
@@ -19,33 +22,69 @@ function Game() {
   function updateCorrect(){
     const newResults = [...results, currentCountry]
     setResults(newResults);
-    
-    const random_value = Math.round(countries.length * Math.random());
-    const chosen_value = countriesLeft[random_value];
-    setCurrentCountry(chosen_value['name']);
-    setCurrentCapital(chosen_value['capital']);
-    setCountriesLeft(countriesLeft.filter((_,index)=>index!==random_value));
+    const myIndex = 0;
+    updateCountry(myIndex);
+    setCountriesLeft(countriesLeft.filter((_,index)=>index!== myIndex));
     setRemaining(remaining-1);
     setInputValue('');
+  }
+
+  function updateCountry(index){
+    const chosen_value = countriesLeft[index];
+    setCurrentCountry(chosen_value['name']);
+    setCurrentCapital(chosen_value['capital']);
+  }
+
+  function handleSkip(){
+    const data = [...countriesLeft];
+    shuffle(data);
+    setCountriesLeft(data);
+    updateCountry(0);
+    setInputValue('');
+  }
+
+  function showResults(){
+    return 0
   }
 
   useEffect(() => {
     axios.get('http://localhost:8000/hello-world/')
         .then(response => {
-            setCountries(response.data.countries);
+          const data = response.data.countries;
+          shuffle(data);
+          setCountries(data);
         })
         .catch(error => {
             console.log(error);
         });
+
+    const handleKeyDown = (event) => {
+      if (event.code === 'Space') {
+        event.preventDefault();
+        setSkipCapital(true);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
 }, []); 
 
   useEffect(()=>{
+    if (skipCapital){
+      handleSkip();
+      setSkipCapital(false);
+    }
+  },[skipCapital]);
+
+
+  useEffect(()=>{
     if (countries.length > 0){
-      const random_value = Math.round(countries.length * Math.random());
-      const chosen_value = countries[random_value];
+      const chosen_value = countries[0];
       setCurrentCountry(chosen_value['name']);
       setCurrentCapital(chosen_value['capital']);
-      const newCountriesLeft = countries.filter((_,index)=>index!==random_value);
+      const newCountriesLeft = countries.filter((_,index)=>index!==0);
       setCountriesLeft(newCountriesLeft);
       setRemaining(countries.length);
     }
@@ -71,17 +110,35 @@ function Game() {
 
   return (
     <div>
-      <Stopwatch isPaused={pauseTimer}></Stopwatch>
-      <h2>Remaining: {remaining}</h2>
-      {countries.length > 0 && (
-        <p>{currentCountry}</p>
-      )}
-      <input onChange={handleInputChange} value={inputValue}></input> <br></br>
-      {results.map((result, index) => (
-      <div key={index}>
-        <p style={{color:'green'}}>{result}</p>
+      <div class='row'>
+        <Stopwatch isPaused={pauseTimer}></Stopwatch>
       </div>
-    ))}
+      <div class='row'>
+        <div class='d-flex justify-content-center'>
+          <h2>Remaining: {remaining}</h2>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='d-flex justify-content-center'>
+          {countries.length > 0 && (
+              <p>{currentCountry}</p>
+              )}
+        </div>
+      </div>
+      <div class='row'>
+        <div class='d-flex justify-content-center'>
+          <input onChange={handleInputChange} value={inputValue}></input> <br></br>
+          <button type="button" class="btn btn-danger">Give up ?</button>
+        </div>
+      </div>
+    <div class='row'>
+
+        {results.map((result, index) => (
+          <div key={index}>
+          <p style={{color:'green'}}>{result}</p>
+        </div>
+      ))}
+      </div>
     </div>
   );
 }
