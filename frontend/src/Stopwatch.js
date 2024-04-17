409b5f8298fdd8fa64baf4dbe61363c265d66956
Looczky <kwiatkowskiluk@protonmail.com>
2024-04-17 23:05:51 +0200

    timer

--- frontend/src/Stopwatch.js
+++ frontend/src/Stopwatch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useStopwatch } from 'react-timer-hook';
 
 function Stopwatch({isPaused},{doReset}) {
@@ -14,18 +15,38 @@ function Stopwatch({isPaused},{doReset}) {
     reset,
   } = useStopwatch({ autoStart: false });
 
-  React.useEffect(()=>{
+  useEffect(()=>{
     if (isPaused) pause();
     else start();
   },[isPaused])
 
-  return (
-    <div style={{textAlign: 'center'}}>
-      <div style={{fontSize: '100px'}}>
-        <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+  if (totalSeconds < 60){
+    return (
+      <div style={{textAlign: 'center'}}>
+        <div style={{fontSize: '100px'}}>
+          <span>{seconds < 10 ? '0'+seconds: seconds}</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else if (totalSeconds >= 60){
+    return (
+      <div style={{textAlign: 'center'}}>
+        <div style={{fontSize: '100px'}}>
+          <span>{minutes < 10 ? '0'+minutes: minutes}</span>:<span>{seconds}</span>
+        </div>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <div style={{fontSize: '100px'}}>
+          <span>{hours < 10 ? '0'+hours:hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Stopwatch;
