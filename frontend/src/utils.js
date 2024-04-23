250f90ebca94c4d6055238ad9538cb780262ffc8
Looczky <32517924+Looczky@users.noreply.github.com>
2024-04-23 13:03:45 +0200

    better new indexing

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,7 +2,7 @@ function shuffle(array) {
     let currentIndex = array.length;
   
     // While there remain elements to shuffle...
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
   
       // Pick a remaining element...
       let randomIndex = Math.floor(Math.random() * currentIndex);
